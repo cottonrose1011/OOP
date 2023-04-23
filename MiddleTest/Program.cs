dc27eac894070efe7b73fbee33c1e9898b33b620
cottonrose1011 <pjeka7er43@gmail.com>
2023-04-23 14:45:57 +0900

    middle test

---
+++ MiddleTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Child();
+            p.Print();
+
+            Child c = (Child)p;
+            c.Print();
+        }
+    }
+    class Parent
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Parent");
+        }
+    }
+
+    class Child : Parent
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Child");
+        }
+    }
+
+}
